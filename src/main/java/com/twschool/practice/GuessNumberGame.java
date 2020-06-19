8d1ddda3955a1aca50e503739c09c0e97db00ee3
fws06118 <1241786550@qq.com>
2020-06-19 19:38:58 +0800

    [fws] should_return_SUCCEND_when_input_1234_given_game_with_answer_1234

--- src/main/java/com/twschool/practice/GuessNumberGame.java
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -1,12 +1,26 @@
 package com.twschool.practice;
 
 public class GuessNumberGame {
+    public static final int MAX_TRY_TIMES = 6;
     private Answer answer;
+    private int lefttryTimes = MAX_TRY_TIMES;
+    private GameStatus gameStatus = GameStatus.CONTITNUE;
     public GuessNumberGame(Answer answer) {
         this.answer = answer;
     }
 
-    public String guess(String userAnswer) {
-        return this.answer.compare(userAnswer);
+    public String  guess(String userAnswer) {
+        lefttryTimes--;
+        String result = answer.compare(userAnswer);
+        if ("4A0B".equals(result)){
+            gameStatus = GameStatus.SUCCEED;
+        }else if (lefttryTimes == 0){
+            gameStatus = GameStatus.FAILED;
+        }
+        return result;
+    }
+
+    public GameStatus getStatus() {
+       return gameStatus;
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberGameTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class GuessNumberGameTest {
+    public static final String SUCCEND = "SUCCEND";
+
     @Test
     public void should_return_4A0B_when_input_1234_given_game_with_answer_1234(){
         //given
@@ -14,5 +16,18 @@ public class GuessNumberGameTest {
 
         Assert.assertEquals("4A0B",result);
     }
-    
+
+    @Test
+    public void should_return_SUCCEND_when_input_1234_given_game_with_answer_1234(){
+        //given
+        Answer answer = new Answer("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+
+        String result = guessNumberGame.guess("1234");
+
+        GameStatus gameStatus = guessNumberGame.getStatus();
+
+        Assert.assertEquals(GameStatus.SUCCEED,gameStatus);
+    }
+
 }
